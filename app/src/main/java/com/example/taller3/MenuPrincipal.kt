77b6c246689093118e8c456c235a72743d84e28a
JuanCor24 <74743148+JuanCor24@users.noreply.github.com>
2024-05-13 14:17:04 -0500

    listview

--- app/src/main/java/com/example/taller3/MenuPrincipal.kt
+++ app/src/main/java/com/example/taller3/MenuPrincipal.kt
@@ -1,7 +1,11 @@
 package com.example.taller3
 
+import com.example.taller3.R
 import android.content.Intent
 import android.os.Bundle
+import android.renderscript.Sampler.Value
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.taller3.databinding.ActivityMenuPrincipalBinding
 import com.example.taller3.databinding.IniciarSesionBinding
@@ -10,6 +14,9 @@ import android.widget.Toast
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 import com.google.firebase.database.database
 
@@ -19,10 +26,13 @@ class MenuPrincipal: AppCompatActivity() {
     private lateinit var binding: ActivityMenuPrincipalBinding
 
 
+
     private lateinit var auth : FirebaseAuth
     private val database = Firebase.database
 
-    private val messageRef = database.getReference("messages")
+    private val messageRef = database.getReference("messages/users")
+
+
 
 
 
@@ -30,8 +40,29 @@ class MenuPrincipal: AppCompatActivity() {
         binding = ActivityMenuPrincipalBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        messageRef.setValue("Hello World!")
 
+        val listView: ListView = findViewById(R.id.listaView)
+
+
+        messageRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userList = mutableListOf<String>()
+                for (userSnapshot in snapshot.children) {
+                    val user = userSnapshot.getValue(User::class.java)
+                    user?.let {
+                        // Agregar el nombre del usuario a la lista
+                        userList.add("${user.nombre} ${user.apellido}")
+                    }
+                }
+                // Crear un adaptador para el ListView y establecerlo
+                val adapter = ArrayAdapter(this@MenuPrincipal, android.R.layout.simple_list_item_1, userList)
+                listView.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MenuPrincipal, "Error al cancelar la lectura de la base de datos", Toast.LENGTH_SHORT).show()
+            }
+        })
 
 
 
